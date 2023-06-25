320724c0d80dc7b40845923a3aa0cb88dde1724b
GabrielMedinaT <116290646+GabrielMedinaT@users.noreply.github.com>
2023-06-25 21:41:45 +0100

    clase 2

---
+++ obtenerDatosDelUsuario.rb
@@ -0,0 +1,9 @@
+print "Ingresa tu nombre: "
+nombre = gets.chomp
+print "Ingresa tu ciudad: "
+ciudad = gets.chomp
+print "Ingresa tu edad: "
+edad = gets.chomp
+puts "Hola " + nombre + " tienes " + edad.to_s + " años" + " y vives en " + ciudad
+
+
